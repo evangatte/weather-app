c7750344e89dc00037b728f1f2e2bae861aff456
Evan Gatte <evangatte2255@gmail.com>
2022-01-31 20:43:37 -0600

    fix unit toggle button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const defaultCity = 'Paris';
 const city = $('#city');
 const clouds = $('#clouds')
 const temperature = $('#temperature');
@@ -8,7 +9,7 @@ const submitButton = $('#submit-button');
 const inputBar = $('#input-bar');
 const unitsButton = $('#units-button')
 const unitText = $('#unit-text')
-inputBar.value = 'Paris';
+
 let units;
 
 
@@ -19,11 +20,12 @@ let units;
 alternateUnits();
 
 function alternateUnits() {
-    unitsButton.click(() => {
-        if (unitText.text() == 'F') {
-            unitText.text('C')
+    unitsButton.click(function () {
+        const localUnitText = $(this).find('#unit-text');
+        if (localUnitText.text() == 'F') {
+            localUnitText.text('K')
         } else {
-            unitText.text('F')
+            localUnitText.text('F')
         }
         submitButton.trigger('click')
     })  
@@ -34,15 +36,22 @@ function alternateUnits() {
 
 
 
-loadWeather('paris')
-
 async function getWeather() {
     if (unitText.text() === 'F') {
         units = 'imperial';
     } else {
         units = '';
     }
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputBar.val()}&appid=0305ee94777d74864f050e119f7de47a&units=${units}`, {mode: 'cors'})
+
+    let cityValue;
+    
+
+    cityValue = inputBar.val().trim() || defaultCity;
+
+
+
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=0305ee94777d74864f050e119f7de47a&units=${units}`, {mode: 'cors'})
     const returnedWeather = await response.json();
     city.text(returnedWeather.name);
      clouds.text(returnedWeather.weather[0].description);
@@ -61,23 +70,7 @@ submitButton.on('click', () => {
 
 })
 
-
-
-async function loadWeather() {
-
-    units = unitText.text()
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputBar.value}&appid=0305ee94777d74864f050e119f7de47a&units=imperial`, {mode: 'cors'})
-    const returnedWeather = await response.json();
-    city.text(returnedWeather.name);
-     clouds.text(returnedWeather.weather[0].description);
-     temperature.text(returnedWeather.main.temp);
-     realFeel.text(returnedWeather.main.feels_like);
-     humidity.text(returnedWeather.main.humidity);
-     wind.text(returnedWeather.wind.speed);
-}
-
-
-
+submitButton.click();
 
 
 
